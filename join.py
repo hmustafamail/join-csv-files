b1a41ca39819df9fd05787713f800cab88c61641
hmustafamail <h.mustafa.mail@gmail.com>
2018-06-27 08:27:51 -0400

    Fixed non-comparability bug
    Now casts join columns to string in both data frames prior to merge; this fixes a bug where one column might be an int and the other a string.

--- join.py
+++ join.py
@@ -14,14 +14,23 @@ import sys
 
 try:
     # Get the command line arguments
+
     in1 = sys.argv[1]
     in2 = sys.argv[2]
     joincol = sys.argv[3]
 
-    # Read, merge, write
+    # Read in the files
     a = pd.read_csv(in1)
     b = pd.read_csv(in2)
+
+    # Turn the joincol to string in both dataframes
+    a[joincol] = a[joincol].astype(str)
+    b[joincol] = b[joincol].astype(str)
+
+    # Merge dataframes
     merged = a.merge(b, on=joincol)
+
+    # Write
     merged.to_csv(in1[:-4] + in2[:-4] + '.csv', index=False)
 
 except:
